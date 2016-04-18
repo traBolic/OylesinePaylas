2c9001cf9b97f6018e99b907df18c1fd19bec977
traBolic <sahindengiz@gmail.com>
2016-04-18 22:35:06 +0300

    http://eodev.com/gorev/8403418 resim 2 uygulama 1 cevabı

--- 2016/04/18/NotKontroluConsoleApp.cs
+++ 2016/04/18/NotKontroluConsoleApp.cs
@@ -6,6 +6,7 @@ namespace NotKontroluConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Öğrenci notunu girin: ");
             int not = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(not < 50 ? "BAŞARISIZ" : "BAŞARILI");
         }
