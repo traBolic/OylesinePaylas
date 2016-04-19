6318cae298395f38082b9dbc975bacb4120a42dc
traBolic <sahindengiz@gmail.com>
2016-04-19 23:03:49 +0300

    http://eodev.com/gorev/8270044 cevabı v2

---
+++ 2016/04/19/BolmeKullanmadanBolme2ConsoleApp.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace BolmeKullanmadanBolme2ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, bolum;
+
+            Console.Write("A: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("B: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            for (bolum = 0; b > 0 && a - b >= 0; bolum++)
+            {
+                a = a - b;
+            }
+
+            Console.Write("\nBölüm=" + bolum + "\nKalan=" + a + "\n\n");
+        }
+    }
+}
