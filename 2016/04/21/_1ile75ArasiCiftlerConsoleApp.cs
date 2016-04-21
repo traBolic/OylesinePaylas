3a3806a18986041a4d9a8ed478acf9d7ceb8b92a
traBolic <sahindengiz@gmail.com>
2016-04-21 11:19:40 +0300

    http://eodev.com/gorev/8416536 cevabı

---
+++ 2016/04/21/_1ile75ArasiCiftlerConsoleApp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _1ile75ArasiCiftlerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 75; i++)
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+        }
+    }
+}
