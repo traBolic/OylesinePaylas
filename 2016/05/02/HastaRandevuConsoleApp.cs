ab1eac0de06a0ec8e3d21e205d293b5b3022721f
traBolic <sahindengiz@gmail.com>
2016-05-02 13:05:06 +0300

    http://eodev.com/gorev/8493956 cevabı

--- 2016/05/02/HastaRandevuConsoleApp.cs
+++ 2016/05/02/HastaRandevuConsoleApp.cs
@@ -11,18 +11,19 @@ namespace HastaRandevuConsoleApp
                    doktor,
                    saat,
                    dakika;
-            Console.Write("Hastanın bilgilerini girin\n\nHastanın adı: ");
-            ad=Console.ReadLine();
-            Console.Write("Hastanın soyadı: ");
-            soyad=Console.ReadLine();
-            Console.Write("Gitmek istediği doktor: ");
-            doktor=Console.ReadLine();
-            Console.Write("Randevu saati (09-17): ");
-            saat=Console.ReadLine();
-            Console.Write("Randevu dakikası (00-59): ");
-            dakika=Console.ReadLine();
 
-            Console.Write("\n\n"+ad+" "+soyad+" isimli hastanın "+saat+":"+dakika+" saatinde, DR. "+doktor+" ile randevusu alınmıştır.\n\n");
+            Console.Write("Hastanın bilgilerini girin\n\nHastanın adı: ");
+            ad = Console.ReadLine();
+            Console.Write("Hastanın soyadı: ");
+            soyad = Console.ReadLine();
+            Console.Write("Gitmek istediği doktor: ");
+            doktor = Console.ReadLine();
+            Console.Write("Randevu saati (09-17): ");
+            saat = Console.ReadLine();
+            Console.Write("Randevu dakikası (00-59): ");
+            dakika = Console.ReadLine();
+
+            Console.Write("\n\n" + ad + " " + soyad + " isimli hastanın " + saat + ":" + dakika + " saatinde, DR. " + doktor + " ile randevusu alınmıştır.\n\n");
         }
     }
 }
