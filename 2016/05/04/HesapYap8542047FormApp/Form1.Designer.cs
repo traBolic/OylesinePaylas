3ecd3a6b44653c93e5937d8ab2013c968ec1de62
traBolic <sahindengiz@gmail.com>
2016-05-04 10:01:44 +0300

    http://eodev.com/gorev/8542047 cevabı

---
+++ 2016/05/04/HesapYap8542047FormApp/Form1.Designer.cs
@@ -0,0 +1,186 @@
+namespace HesapYap8542047FormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnArti = new System.Windows.Forms.Button();
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Sayı";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Font = new System.Drawing.Font("Calibri", 20F);
+            this.txtSayi1.Location = new System.Drawing.Point(12, 39);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(260, 40);
+            this.txtSayi1.TabIndex = 1;
+            this.txtSayi1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Font = new System.Drawing.Font("Calibri", 20F);
+            this.txtSayi2.Location = new System.Drawing.Point(12, 126);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(260, 40);
+            this.txtSayi2.TabIndex = 3;
+            this.txtSayi2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. Sayı";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnArti
+            // 
+            this.btnArti.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnArti.Font = new System.Drawing.Font("Calibri", 25F);
+            this.btnArti.Location = new System.Drawing.Point(12, 203);
+            this.btnArti.Name = "btnArti";
+            this.btnArti.Size = new System.Drawing.Size(47, 56);
+            this.btnArti.TabIndex = 4;
+            this.btnArti.Text = "+";
+            this.btnArti.UseVisualStyleBackColor = true;
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEksi.Font = new System.Drawing.Font("Calibri", 25F);
+            this.btnEksi.Location = new System.Drawing.Point(82, 203);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(47, 56);
+            this.btnEksi.TabIndex = 5;
+            this.btnEksi.Text = "-";
+            this.btnEksi.UseVisualStyleBackColor = true;
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCarp.Font = new System.Drawing.Font("Calibri", 25F);
+            this.btnCarp.Location = new System.Drawing.Point(152, 203);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(47, 56);
+            this.btnCarp.TabIndex = 6;
+            this.btnCarp.Text = "*";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            // 
+            // btnBol
+            // 
+            this.btnBol.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBol.Font = new System.Drawing.Font("Calibri", 20F);
+            this.btnBol.Location = new System.Drawing.Point(225, 203);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(47, 56);
+            this.btnBol.TabIndex = 7;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Enabled = false;
+            this.txtSonuc.Font = new System.Drawing.Font("Calibri", 20F);
+            this.txtSonuc.Location = new System.Drawing.Point(12, 320);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.ReadOnly = true;
+            this.txtSonuc.Size = new System.Drawing.Size(260, 40);
+            this.txtSonuc.TabIndex = 8;
+            this.txtSonuc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 27);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sonuç";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 372);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnEksi);
+            this.Controls.Add(this.btnArti);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 410);
+            this.MinimumSize = new System.Drawing.Size(300, 410);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnArti;
+        private System.Windows.Forms.Button btnEksi;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCarp;
+    }
+}
