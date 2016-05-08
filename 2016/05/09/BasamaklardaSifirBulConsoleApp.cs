8cc04a9282bfbe29ce164bf898eeed10f88c2485
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-09 01:19:25 +0300

    http://eodev.com/gorev/8578342 cevabı

--- 2016/05/09/BasamaklardaSifirBulConsoleApp.cs
+++ 2016/05/09/BasamaklardaSifirBulConsoleApp.cs
@@ -11,11 +11,11 @@ namespace BasamaklardaSifirBulConsoleApp
             {
                 Console.Write("Sayı gir: ");
                 deger = Console.ReadLine();
-                for (int i = deger.Length - 1, j = 0, k = 1; i >= 0 && j < 1; i--, k++)
+                for (int i = deger.Length - 1, j = 0, k = 1; i >= 0 && j < 1 && deger != "0"; i--, k++)
                     if (deger[i] == '0')
                     {
                         j++;
-                        Console.WriteLine(k + ". basamak 0");
+                        Console.Write("\n" + k + ". basamak 0\n\n");
                     }
             }
         }
