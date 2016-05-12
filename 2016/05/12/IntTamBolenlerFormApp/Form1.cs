cfbbd1e45bac5ae8b91b6d4afd6d736cb33e067d
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-12 17:22:03 +0300

    http://eodev.com/gorev/8614399 cevabı

---
+++ 2016/05/12/IntTamBolenlerFormApp/Form1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Forms;
+
+namespace IntTamBolenlerFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        decimal oncekiDeger;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal sayi = numSayi.Value;
+            if (oncekiDeger != sayi)
+            {
+                listSayilar.Items.Clear();
+                for (int i = 1; i <= sayi; i++)
+                    if (sayi % i == 0)
+                        listSayilar.Items.Add(i + "");
+                oncekiDeger = sayi;
+            }
+        }
+    }
+}
+
+//http://eodev.com/gorev/8614399
