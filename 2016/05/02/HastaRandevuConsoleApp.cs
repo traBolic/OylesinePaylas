339beef018a6c37ba5c4043844f0ed2ea6ed9381
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-19 13:57:01 +0300

    http://eodev.com/gorev/8493956 cevabı

--- 2016/05/02/HastaRandevuConsoleApp.cs
+++ 2016/05/02/HastaRandevuConsoleApp.cs
@@ -8,11 +8,32 @@ namespace HastaRandevuConsoleApp
         {
             string ad,
                    soyad,
+                   bolum,
                    doktor,
+                   tarih,
                    saat,
-                   dakika;
+                   ilaclar;
 
-            Console.Write("Hastanın bilgilerini girin\n\nHastanın adı: ");
+            Console.Write("Lütfen hasta bilgilerini girin\n\nHastanın adı: ");
+            ad = Console.ReadLine();
+            Console.Write("Hastanın soyadı: ");
+            soyad = Console.ReadLine();
+            Console.Write("Gitmek istediği bölüm: ");
+            bolum = Console.ReadLine();
+            Console.Write("Gitmek istediği doktor: ");
+            doktor = Console.ReadLine();
+            Console.Write("Randevu tarihi (01.01.2016): ");
+            tarih = Console.ReadLine();
+            Console.Write("Randevu saati (09:00 ve 17:30 arası): ");
+            saat = Console.ReadLine();
+            Console.Write("Varsa kullandığınız ilaçlar(virgül ile ayırın): ");
+            ilaclar = Console.ReadLine();
+
+            Console.Write("\n\n>  Sayın " + ad + " " + soyad + ";\n   Girdiğiniz bilgiler ile, "+tarih+" tarihinde ve " + saat + " saatinde, DR. " + doktor + " ile randevunuz alınmıştır.\n\n");*/
+
+            
+            /* Eski kod
+             Console.Write("Hastanın bilgilerini girin\n\nHastanın adı: ");
             ad = Console.ReadLine();
             Console.Write("Hastanın soyadı: ");
             soyad = Console.ReadLine();
@@ -23,7 +44,9 @@ namespace HastaRandevuConsoleApp
             Console.Write("Randevu dakikası (00-59): ");
             dakika = Console.ReadLine();
 
-            Console.Write("\n\n" + ad + " " + soyad + " isimli hastanın " + saat + ":" + dakika + " saatinde, DR. " + doktor + " ile randevusu alınmıştır.\n\n");
+            Console.Write("\n\n" + ad + " " + soyad + " isimli hastanın " + saat + ":" + dakika + " saatinde, DR. " + doktor + " ile randevusu alınmıştır.\n\n");*/
         }
     }
 }
+
+//http://eodev.com/gorev/8493956
