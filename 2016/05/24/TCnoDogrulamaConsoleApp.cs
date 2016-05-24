9898615ef541b1db51db014feafcf11f71fa57ea
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-24 15:26:25 +0300

    http://eodev.com/gorev/8709328 cevabı

--- 2016/05/24/TCnoDogrulamaConsoleApp.cs
+++ 2016/05/24/TCnoDogrulamaConsoleApp.cs
@@ -11,13 +11,17 @@ namespace TCnoDogrulamaConsoleApp
 
             Console.Write("T.C. Kimlik numarası gir: ");
             tcno = Console.ReadLine();
+            if (tcno.Length == 11)
+            {
+                for (i = 0; i < 10; i++)
+                    topla += Convert.ToInt32(tcno[i].ToString());
 
-            for (i = 0; i < 10; i++)
-                topla += Convert.ToInt32(tcno[i].ToString());
-
-            mesaj = "Hatalı kimlik numarası";
-            if (topla % 10 == Convert.ToInt32(tcno[10].ToString()))
-                mesaj = "Doğru kimlik numarası";
+                mesaj = "Hatalı kimlik numarası";
+                if (topla % 10 == Convert.ToInt32(tcno[10].ToString()))
+                    mesaj = "Doğru kimlik numarası";
+            }
+            else
+                mesaj = "TC kimlik numarası 11 basamaklı olmalıdır";
 
             Console.WriteLine("\n" + mesaj + "\n");
         }
