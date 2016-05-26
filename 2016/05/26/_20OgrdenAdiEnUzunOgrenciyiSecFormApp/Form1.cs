687a2440b57c8b94b392be2be95cd2bed5a52eb0
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-26 15:09:39 +0300

    http://eodev.com/gorev/8726293 cevabı

---
+++ 2016/05/26/_20OgrdenAdiEnUzunOgrenciyiSecFormApp/Form1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Windows.Forms;
+
+namespace _20OgrdenAdiEnUzunOgrenciyiSecFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBul_Click(object sender, EventArgs e)
+        {
+            string[] ogrenciler = txtOgrenciler.Text.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            string mesaj = "";
+            if (ogrenciler.Length == 10)
+            {
+                int buyuk = 0;
+                for (int i = 0; i < 10; i++)
+                    if (ogrenciler[i].Length > buyuk)
+                        buyuk = i;
+                mesaj = ogrenciler[buyuk];
+            }
+            else
+                mesaj = "10 öğrenci olmalı";
+            txtSonuc.Text = mesaj;
+        }
+    }
+}
+//http://eodev.com/gorev/8726293
