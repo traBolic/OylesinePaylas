6ee49dc37414f024a432fc083df0f4a893957b6e
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-26 16:47:45 +0300

    http://eodev.com/gorev/8726293 cevabı

--- 2016/05/26/_10OgrdenAdiEnUzunOgrenciyiSecFormApp/Form1.cs
+++ 2016/05/26/_10OgrdenAdiEnUzunOgrenciyiSecFormApp/Form1.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Windows.Forms;
 
-namespace _20OgrdenAdiEnUzunOgrenciyiSecFormApp
+namespace _10OgrdenAdiEnUzunOgrenciyiSecFormApp
 {
     public partial class Form1 : Form
     {
