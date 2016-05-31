bdb997341e3bbd5df796a465b87c3b46b1935787
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-31 17:57:41 +0300

    http://eodev.com/gorev/8757808

---
+++ 2016/05/31/DikdortgenAlanHesaplaConsoleApp.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace DikdortgenAlanHesaplaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d_uk,
+                   d_kk,
+                   alan;
+            string s_uk = "",
+                   s_kk = "";
+
+            while (true)
+            {
+                if (s_uk == "" || s_uk == "0")
+                {
+                    Console.Write("Uzun kenar gir: ");
+                    s_uk = Console.ReadLine();
+                }
+                else if (s_kk == "" || s_kk == "0")
+                {
+                    Console.Write("Kısa kenar gir: ");
+                    s_kk = Console.ReadLine();
+                }
+                else
+                    break;
+            }
+
+            d_uk = Convert.ToDouble(s_uk);
+            d_kk = Convert.ToDouble(s_kk);
+            alan = d_uk * d_kk;
+
+            Console.Write("\nBelirlenen dikdörtgenin alanı: " + alan + "\n\n");
+        }
+    }
+}
+//http://eodev.com/gorev/8757808
