f6626740edc629b31754bb206278c877e1a5b8c4
Şahin DNZ <traBolic@users.noreply.github.com>
2016-05-31 23:04:09 +0300

    http://eodev.com/gorev/8760406 cevabı

---
+++ 2016/05/31/_20IntTekCiftSifirSayConsoleApp.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace _20IntTekCiftSifirSayConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sifir = 0,
+                tek = 0,
+                cift = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write((i+1)+". sayıyı gir: ");
+                string deger = Console.ReadLine();
+                if (deger != "")
+                {
+                    int sayi = Convert.ToInt32(deger);
+                    if (sayi == 0)
+                        sifir++;
+                    if (sayi % 2 == 0)
+                        cift++;
+                    if (sayi % 2 == 1)
+                        tek++;
+                }
+                else
+                    i--;
+            }
+            Console.Write("\nGirilen sayılarda: " + sifir + " adet sıfır," +
+                          "\n\t\t   " + cift + " adet çift," +
+                          "\n\t\t   " + tek + " adet tek vardır.\n\n");
+        }
+    }
+}
+//http://eodev.com/gorev/8760406
