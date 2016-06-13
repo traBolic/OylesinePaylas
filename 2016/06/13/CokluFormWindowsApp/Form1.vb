6b610746ce72aced79aa729cdf37491aa654e0dd
Şahin DNZ <traBolic@users.noreply.github.com>
2016-06-13 15:41:24 +0300

    http://eodev.com/gorev/8801495 cevabı

--- 2016/06/13/CokluFormWindowsApp/Form1.vb
+++ 2016/06/13/CokluFormWindowsApp/Form1.vb
@@ -1,12 +1,21 @@
 Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Form2.Show()
-    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form2.Label1.Show()
+        Form2.Show()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form2.Hide()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Form2.Visible Then
+            Form2.Label1.Show()
+        Else
+            MessageBox.Show("Önce Form2 açılmalıdır")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Form2.Label1.Hide()
     End Sub
 End Class
