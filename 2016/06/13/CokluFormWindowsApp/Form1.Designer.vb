b12e437eaee5d8dcd5faf4f9b1dda5e00ba507c1
Şahin DNZ <traBolic@users.noreply.github.com>
2016-06-13 15:41:56 +0300

    http://eodev.com/gorev/8801495 cevabı

--- 2016/06/13/CokluFormWindowsApp/Form1.Designer.vb
+++ 2016/06/13/CokluFormWindowsApp/Form1.Designer.vb
@@ -24,13 +24,17 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.Button1.Font = New System.Drawing.Font("Calibri", 15.0!)
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Location = New System.Drawing.Point(12, 47)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(108, 48)
         Me.Button1.TabIndex = 0
@@ -41,18 +45,64 @@ Partial Class Form1
         '
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.Button2.Font = New System.Drawing.Font("Calibri", 15.0!)
-        Me.Button2.Location = New System.Drawing.Point(164, 12)
+        Me.Button2.Location = New System.Drawing.Point(164, 47)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(108, 48)
         Me.Button2.TabIndex = 0
         Me.Button2.Text = "Gizle"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 15.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 150)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(260, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Merhaba yazısı"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button3.Font = New System.Drawing.Font("Calibri", 15.0!)
+        Me.Button3.Location = New System.Drawing.Point(12, 191)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(108, 48)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "Göster"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button4.Font = New System.Drawing.Font("Calibri", 15.0!)
+        Me.Button4.Location = New System.Drawing.Point(164, 191)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(108, 48)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "Gizle"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 15.0!)
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(260, 23)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Form2 Penceresi"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 72)
+        Me.ClientSize = New System.Drawing.Size(284, 251)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -66,4 +116,8 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label2 As Label
 End Class
