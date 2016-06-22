37930d830e5efa907625c8552f8ded26b2afef9d
Şahin DNZ <traBolic@users.noreply.github.com>
2016-06-22 21:38:46 +0300

    http://eodev.com/gorev/8604148 cevabı

--- 2016/06/22/EtkilesimliIleVeritabani/veritabani.sql
+++ 2016/06/22/EtkilesimliIleVeritabani/veritabani.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `etkilesimliileveritabani` (
-  `islem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `kayit_id` int(11) NOT NULL AUTO_INCREMENT,
   `ad` varchar(25) NOT NULL,
   `soyad` varchar(25) NOT NULL,
   `eposta` varchar(255) NOT NULL,
-  `tel` int(11) DEFAULT NULL,
+  `tel` int(11) DEFAULT NOT NULL,
   `mesaj` text NOT NULL,
   `tarih` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`islem_id`)
+  PRIMARY KEY (`kayit_id`)
 )
