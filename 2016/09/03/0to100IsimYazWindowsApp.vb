dd88aa1bdf9b39f494b422aed1147a4bbacac332
Şahin DNZ <traBolic@users.noreply.github.com>
2016-09-03 11:14:40 +0300

    http://eodev.com/gorev/8911336 cevabı

---
+++ 2016/09/03/0to100IsimYazWindowsApp.vb
@@ -0,0 +1,9 @@
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        For i As Integer = 0 To 100
+            TextBox1.Text += "Kirito" & vbCrLf
+        Next
+
+    End Sub
+End Class
