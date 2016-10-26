a54e0bf589530710f16450e8d167c8e388be87af
Şahin DNZ <traBolic@users.noreply.github.com>
2016-10-26 20:51:28 +0300

    http://eodev.com/gorev/9346862 cevabı

---
+++ 2016/10/26/XCut/MyForm.cpp
@@ -0,0 +1,12 @@
+#include <limits.h>
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	XCut::MyForm form;
+	Application::Run(%form);
+}
